193870daae7100b2054a085bc9324e9bad5e9d90
tahir6301 <talalshaik7@gmail.com>
2024-05-23 07:47:03 +0530

    classes

---
+++ php/first.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+</head>
+<body>
+
+
+<div>
+<?php
+
+$india =1947; //variable
+
+$india = "New Bharat"; //string
+$value = 2882;// integer
+$float = 393.333;//float
+$i = true;//boolean
+echo $float;
+print "Hello"."India <br>";
+echo "Hello"."world ". "$india" . "<br>";
+?>
+</div>
+
+<div>
+    <h4 style="color:red">
+<?php
+print "Hello"."India {$value}<br>";
+echo "Hello"."world {$india}" . "<br>";
+echo $i = true;
+?>
+
+</h4> 
+</div> 
+</body>
+</html>
---
+++ php/first2.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$india =1947; //variable
+
+$india = "New Bharat"; //string
+$value = 2882;// integer
+$float = 393.333;//float
+$i = true;//boolean
+
+print "Hello"."India <br>";
+echo "Hello"."world ". "$india" . "<br>";
+print "Hello"."India {$value}<br>";
+echo "Hello"."world {$india}" . "<br>";
+?>
